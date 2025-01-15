9cb08fd7504d53cd3e0d7c390e854c3f52d5e441
knyazkin maxim <knyazkinmaxim626@gmail.com>
2025-01-15 13:51:38 +0300

    reveal done

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,26 +1,19 @@
 let reveals = Array.from(document.querySelectorAll(".reveal"));
 
-window.scroll(10, 100);
+window.addEventListener("scroll", function () {
+  reveals.forEach((el) => {
+    findElem(el);
+  });
+});
 
-//--------------------------------------------------
-// for (el of reveals) {
-//   const { top, bottom, x, y } = el.getBoundingClientRect();
-//   setInterval(() => {
-//     console.log(top, bottom, x, y);
-//   }, 3000);
-//   el.classList.add("reveal_active");
-// }
-//---------------------------------------------------
-// function isVisible(el) {
-//   const { top, bottom } = el.getBoundingClientRect();
+function findElem(el) {
+  const { top, bottom } = el.getBoundingClientRect();
 
-//   if (bottom < 0) {
-//     el.classList.remove("reveal_active");
-//   }
-
-//   if (top > window.innerHeight) {
-//     el.classList.remove("reveal_active");
-//   }
-
-//   el.classList.add("reveal_active");
-// }
+  if (bottom > 0 && top < window.innerHeight) {
+    el.classList.add("reveal_active");
+    console.log("Элемент видно");
+  } else {
+    el.classList.remove("reveal_active");
+    console.log("Элемент НЕ видно");
+  }
+}
