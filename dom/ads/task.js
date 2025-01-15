56346fae4d273f95faaaeeee88f92ac3e820add6
knyazkin maxim <knyazkin.maxim.626@gmail.com>
2025-01-15 21:30:27 +0300

    ads done

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,13 +1,16 @@
 let rotators = Array.from(document.querySelectorAll(".rotator__case"));
 
 function changeAdd() {
-  for (let i = 0; i < rotators.length; i++) {
-    console.log(rotators[i]);
-    console.log(rotators[i + 1]);
+  let active = document.querySelector(".rotator__case_active");
+  let next = active.nextElementSibling;
+
+  if (!next || !next.classList.contains('rotator__case')) {
+    next = rotators[0];
   }
+
+  active.classList.remove("rotator__case_active");
+  next.classList.add("rotator__case_active");
+
 }
 
 setInterval(changeAdd, 1000);
-
-// rotators[i].classList.remove("rotator__case_active");
-// rotators[i + 1].classList.add("rotator__case_active");
